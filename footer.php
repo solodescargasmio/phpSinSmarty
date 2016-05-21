3822b069e4bcb3b14be698976c752dfd21e18d7f
Yo <Yo@pico>
2016-05-21 20:01:27 +0000

    Final, para presentar.

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@ and open the template in the editor.
 -->
  <link href="./css/dashboard.css" rel="stylesheet">
    <footer class="footer">
-       <font style="font-family: Times New Roman; size: 8;">  <p>Tecnólogo en Informática
+       <font style="font-family: Times New Roman; size: 6; color: #000;">  <p>Tecnólogo en Informática
            <br> Universidad de la República
     <br>CETP-UTU
     2016</p></font>

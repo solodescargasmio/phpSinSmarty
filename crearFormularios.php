b6ba7cf6bb6a75edb3c6b9e6a952828d15deaca7
Yo <Yo@pico>
2016-07-08 01:00:04 +0000

    Listo

--- crearFormularios.php
+++ crearFormularios.php
@@ -183,7 +183,7 @@ function capitalize(s)//convierte minusculas a Mayusculas
           <div id="menus">
                 <a href="#" onclick="mostrarDiv()"> <button id="mostrar"  class="btn btn-primary btn-group-sm">Agregar Atributo</button></a>
    <a href="#" onclick="mostrarDiv()"> <button id="ocultar"  class="btn btn-primary btn-group-sm">Ocultar Tabla de Atributos</button></a>
-   <a href="nuevaVersion.php" > <button id="ver"  class="btn btn-primary btn-group-sm">Nueva Versión</button></a> 
+   <a href="version.php" > <button id="ver"  class="btn btn-primary btn-group-sm">Nueva Versión</button></a> 
   <!-- Buscar atributo <input type="search" name="buscaratr" id="buscaratr">-->  
    <form id="miform" class="form-horizontal"  method="post" enctype="multipart/form-data">
           <br> <table class="table-responsive" border="1">  
